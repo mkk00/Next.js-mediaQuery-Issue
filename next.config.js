f937aee6734aecb9ed6fceeef41399b1218d6ff3
mkk <mk1210k@gmail.com>
2024-01-26 02:15:48 +0900

    next.config 절대 경로 설정 추가

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const path = require("path")
 
-module.exports = nextConfig
+module.exports = {
+  webpack(config) {
+    config.resolve.alias["@"] = path.resolve(__dirname)
+    config.resolve.alias["@components"] = path.resolve(__dirname, "components")
+    return config
+  },
+}
