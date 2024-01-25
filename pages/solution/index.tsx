dc978bb9e0f06fc68f12aa0b44058a20abc5a6af
mkk <mk1210k@gmail.com>
2024-01-26 02:20:08 +0900

    이슈 해결 페이지 추가 및 useMediaQuery 훅 리팩토링 적용

--- pages/solution/index.tsx
+++ pages/solution/index.tsx
@@ -1,9 +1,40 @@
+import { useIsMobile } from "@/hooks/useMediaQuery-Refact"
 import styled from "@emotion/styled"
+import { useEffect, useState } from "react"
 
-export default function Issue() {
-  return <Div>이슈 해결 페이지입니다.</Div>
+export default function Solution() {
+  const isMobile = useIsMobile()
+
+  return (
+    <Div isMobile={isMobile}>
+      <div>
+        이슈가 해결된 페이지입니다.
+        <br />
+        isMount 가 true 일 때만 컴포넌트를 렌더링합니다.
+        <br />
+        모바일 브라우저로 변경한 후 새로고침해보세요.
+      </div>
+    </Div>
+  )
 }
 
-const Div = styled.div`
-  font-weight: 600;
+const Div = styled.div<{ isMobile: boolean }>`
+  background-color: #eaeaea;
+  padding: ${(props) => (props.isMobile ? "20px" : "0")};
+  & > div {
+    margin-bottom: 10px;
+  }
 `
+
+export function Solution2() {
+  const isMobile = useIsMobile()
+  const [isMount, setIsMount] = useState(false)
+
+  useEffect(() => {
+    setIsMount(true)
+  }, [])
+
+  return (
+    <>{isMount && <Div isMobile={isMobile}>두번째 해결 방법입니다.</Div>}</>
+  )
+}
